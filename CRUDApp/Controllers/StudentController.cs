10272b15b709293fc3383126c6a71c1534991e6b
Daren Nkomo <daren.nkomo@gmail.com>
2019-07-27 19:44:49 -0600

    CRUD working

--- CRUDApp/Controllers/StudentController.cs
+++ CRUDApp/Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,10 +25,47 @@ namespace CRUDApp.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult Create(student tbl)
-        //{
+        [HttpPost]
+        public ActionResult Create(student tbl)
+        {
+            ObjectParameter para = new ObjectParameter("idout", typeof(int));
+            int result = db.sp_insertupdate_student(tbl.id, tbl.firstName, tbl.lastName, tbl.homeAddress, tbl.phoneNumber, para);
+            this.db.SaveChanges();
 
-        //}
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            sp_viewStudentById_Result obj = new sp_viewStudentById_Result();
+            obj = db.sp_viewStudentById(id).FirstOrDefault();
+            return View(obj);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(student tbl)
+        {
+            ObjectParameter para = new ObjectParameter("idout", typeof(int));
+            int result = db.sp_insertupdate_student(tbl.id, tbl.firstName, tbl.lastName, tbl.homeAddress, tbl.phoneNumber, para);
+            this.db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            sp_viewStudentById_Result obj = new sp_viewStudentById_Result();
+            obj = db.sp_viewStudentById(id).FirstOrDefault();
+            return View(obj);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            int result = db.sp_deleteStudentByID(id);
+            this.db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- CRUDApp/Models/StudentModel.Context.cs
+++ CRUDApp/Models/StudentModel.Context.cs
@@ -33,5 +33,48 @@ namespace CRUDApp.Models
         {
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_ViewAllStudent_Result>("sp_ViewAllStudent");
         }
+    
+        public virtual int sp_insertupdate_student(Nullable<int> id, string firstName, string lastName, string homeAddress, Nullable<long> phoneNumber, ObjectParameter idout)
+        {
+            var idParameter = id.HasValue ?
+                new ObjectParameter("id", id) :
+                new ObjectParameter("id", typeof(int));
+    
+            var firstNameParameter = firstName != null ?
+                new ObjectParameter("firstName", firstName) :
+                new ObjectParameter("firstName", typeof(string));
+    
+            var lastNameParameter = lastName != null ?
+                new ObjectParameter("lastName", lastName) :
+                new ObjectParameter("lastName", typeof(string));
+    
+            var homeAddressParameter = homeAddress != null ?
+                new ObjectParameter("homeAddress", homeAddress) :
+                new ObjectParameter("homeAddress", typeof(string));
+    
+            var phoneNumberParameter = phoneNumber.HasValue ?
+                new ObjectParameter("phoneNumber", phoneNumber) :
+                new ObjectParameter("phoneNumber", typeof(long));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_insertupdate_student", idParameter, firstNameParameter, lastNameParameter, homeAddressParameter, phoneNumberParameter, idout);
+        }
+    
+        public virtual ObjectResult<sp_viewStudentById_Result> sp_viewStudentById(Nullable<int> id)
+        {
+            var idParameter = id.HasValue ?
+                new ObjectParameter("id", id) :
+                new ObjectParameter("id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_viewStudentById_Result>("sp_viewStudentById", idParameter);
+        }
+    
+        public virtual int sp_deleteStudentByID(Nullable<int> id)
+        {
+            var idParameter = id.HasValue ?
+                new ObjectParameter("id", id) :
+                new ObjectParameter("id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_deleteStudentByID", idParameter);
+        }
     }
 }
