3fe6b5c181cfcc62480bf95e4a2bf9906f09f279
Daren Nkomo <daren.nkomo@gmail.com>
2019-07-27 18:05:33 -0600

    index of student works

---
+++ CRUDApp/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUDApp.Models;
+
+namespace CRUDApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentEntities db = new StudentEntities();
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<sp_ViewAllStudent_Result> result = new List<sp_ViewAllStudent_Result>();
+            result = db.sp_ViewAllStudent().ToList();
+            return View(result);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Create(student tbl)
+        //{
+
+        //}
+    }
+}
